b5803840c095d2f290f7f974f1d80ab6b095377f
luisfpaula <luis.f.paula@gmail.com>
2016-06-17 17:41:31 -0300

    Teste

---
+++ client/routes/user.js
@@ -0,0 +1,17 @@
+Router.route("/user/:_id", function(){
+
+            var _id = this.params._id;
+            var isFollowing = Friendships.isFollowing(_id);
+            Session.set("currentUserId", _id);
+            Session.set("isFollowing", isFollowing);
+            this.render("user", {
+                data: function(){            
+                return {
+                    user: Meteor.users.findOne({_id: _id}),
+                    posts: Posts.list([_id]),
+                    followers: Friendships.followers(_id),
+                    followings: Friendships.followings(_id)                
+                }
+            }
+        });
+}, {name: "user"});
---
+++ server/methods.js
@@ -0,0 +1,27 @@
+Meteor.methods({
+    followUser: function (friendId) {
+        Friendships.follow(friendId);
+    },
+    unfollowUser: function (friendId) {
+        Friendships.unfollow(friendId);
+    },
+    profileUpdate: function (name, about) {
+        Meteor.users.update(
+            {_id: Meteor.userId()},
+            {$set: {
+                "profile.name": name,
+                "profile.about": about
+            }}
+        );
+
+        Posts.update(
+            { userId: Meteor.userId()},
+            { $set: {name: name}},
+            { multi: true}
+        );
+    },
+
+    publishPost: function(message, name){
+        Posts.publish(message, name);
+    }
+});
